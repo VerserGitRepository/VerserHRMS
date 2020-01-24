3f4e94db50f336135fa78f4ba79bd5d07e8f31d0
KalyanandaVedula <bharatvedula@gmail.com>
2020-01-24 17:07:45 +1100

    hrms changes

--- VerserHRManagement/App_Start/BundleConfig.cs
+++ VerserHRManagement/App_Start/BundleConfig.cs
@@ -9,24 +9,26 @@ namespace VerserHRManagement
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-jquery-1.12.4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                  "~/Scripts/DataTables/jquery.dataTables.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/datatables.css"));
+                  "~/Content/bootstrap.css",
+                  "~/Content/site.css",
+                  "~/Content/DataTables/css/jquery.dataTables.css",
+                  "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
