b09a816d1310bb7cbc025ade8cbc34ee2e8963af
KalyanandaVedula <bharatvedula@gmail.com>
2020-02-03 14:26:23 +1100

    ddl changes

--- VerserHRManagement/Controllers/CandidatesController.cs
+++ VerserHRManagement/Controllers/CandidatesController.cs
@@ -29,12 +29,12 @@ namespace VerserHRManagement
             model.EmploymentList = new SelectList(ListItemService.EmploymentTypeList().Result, "ID", "Value");
             model.WarehouseNameList = new SelectList(ListItemService.Warehouses().Result, "ID", "Value");
             model.PayFrequencyList = new SelectList(ListItemService.PayFrequency().Result, "ID", "Value");
-            model.TechnicianLevelList = new SelectList(ListItemService.TechnicianLevel().Result, "ID", "Value");
+            
+           
             model.AssignResourceList = new SelectList(ListItemService.AssignResources().Result, "ID", "Value");
             model.EmployeeStatusList = new SelectList(ListItemService.EmployeeStatusSet().Result, "ID", "Value");
             model.WorkRightsList = new SelectList(ListItemService.WorkRights().Result, "ID", "Value");
-            model.ResourceCategoriesList = new SelectList(ListItemService.ResourceCategories().Result, "ID", "Value");
-            model.DrivingLicensesList = new SelectList(ListItemService.DrivingLicenses().Result, "ID", "Value");
+          
             return View(model);
         }
         public ActionResult Details(int id)
