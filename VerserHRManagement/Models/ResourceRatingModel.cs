4708a379b7a6f818de543457cad1ebec149a013b
RaginiMalhotra <RaginiMalhotra@DESKTOP-T12Q553>
2020-02-21 15:28:01 +1100

    UI changes

---
+++ VerserHRManagement/Models/ResourceRatingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerserHRManagement.Models
+{
+    public class ResourceRatingModel
+    {
+        public int Id { get; set; }
+        public int CandidateTimeSheetID { get; set; }
+        public int Punctuality { get; set; }
+        public int ProfessionalService { get; set; }
+        public int PoliteAndCourteous { get; set; }
+        public int WorkQuality { get; set; }
+        public string Comments { get; set; }
+        public string User { get; set; }
+    }
+}
