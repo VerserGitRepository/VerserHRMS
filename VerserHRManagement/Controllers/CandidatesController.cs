90455b9b53c01c8c42ae7c1ef8f052a622f9d9f2
KalyanandaVedula <bharatvedula04@gmail.com>
2020-06-03 15:46:39 +1000

    HRMS Changes

--- VerserHRManagement/Controllers/CandidatesController.cs
+++ VerserHRManagement/Controllers/CandidatesController.cs
@@ -39,7 +39,14 @@ namespace VerserHRManagement
             model.EmployeeStatusList = new SelectList(ListItemService.EmployeeStatusSet().Result, "ID", "Value");
             model.WorkRightsList = new SelectList(ListItemService.WorkRights().Result, "ID", "Value");
             model.ResourceCategoriesList = new SelectList(ListItemService.ResourceCategories().Result, "ID", "Value");
-            model.DrivingLicensesList = new SelectList(ListItemService.DrivingLicenses().Result, "ID", "Value");         
+            model.DrivingLicensesList = new SelectList(ListItemService.DrivingLicenses().Result, "ID", "Value");
+            //model.CandidateNameList = new List<ListItemViewModel>();
+            List<SelectListItems> model1 = new List<SelectListItems>();
+            foreach (var candidate in model.CandidateList)
+            {
+                model1.Add(new SelectListItems { ID = candidate.ID, Name = candidate.CandidateName });// = new SelectList(model.CandidateList.Select(c => c.CandidateName), model.CandidateList.Select(c => c.ID), model.CandidateList.Select(c => c.ID));
+            }
+            model.CandidateNameList = new SelectList(model1, "ID", "Name");
             return View(model);
         }
         public ActionResult Details(int id)
